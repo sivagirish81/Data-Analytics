368d5bb02830fda3fb468fa761f215a44f209869
Siva Girish <sivagirish81@gmail.com>
2019-10-17 03:31:05 +0530

    .

--- Assignment-5/Time-Series.r
+++ Assignment-5/Time-Series.r
@@ -1,13 +1,13 @@
 
 
 Sensex <- read.csv("BSESN.csv")
-str(Sensex)
+#str(Sensex)
 
 Sensex<-Sensex[,colnames(Sensex)!="Adj.Close"]
-str(Sensex)
+#str(Sensex)
 
 Sensex<-na.omit(Sensex)
-head(Sensex)
+#head(Sensex)
 
 
 
@@ -19,12 +19,13 @@ LC<-log(Close_Values)
 plot.ts(LC)
 CHWF1<-HoltWinters(Close_Values,beta=NULL,gamma=NULL)
 plot(CHWF1)
+CHWF1
 CHWF1$SSE
 #SSE = 12195092
 
-CHWF2<-HoltWinters(LC,beta=NULL,gamma=NULL)
-plot(CHWF2)
-CHWF2$SSE
+#CHWF2<-HoltWinters(LC,beta=NULL,gamma=NULL)
+#plot(CHWF2)
+#CHWF2$SSE
 
 #SSE = 46.45982
 
@@ -35,7 +36,7 @@ fc<-forecast(CHWF1,h=365)
 plot(fc)
 accuracy(fc)
 
-fc1<-forecast(CHWF2,h=730)
+fc1<-forecast(CHWF1,h=730)
 
 plot(fc1)
 accuracy(fc1)
